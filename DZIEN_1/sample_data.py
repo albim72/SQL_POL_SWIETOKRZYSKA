bfc264157ffc14990391fb2b0ec40c9a761df746
albim72 <marcin.albiniak72@gmail.com>
2026-03-26 10:22:54 +0100

    Create sample_data.py

---
+++ DZIEN_1/sample_data.py
@@ -0,0 +1,20 @@
+users = [
+    {
+        "user_id": 1,
+        "name": "Anna Kowalska",
+        "email": "anna@example.com",
+        "addresses": [
+            {"city": "Kraków", "street": "Lipowa 5"}
+        ],
+        "interests": ["AI", "databases", "running"]
+    },
+    {
+        "user_id": 2,
+        "name": "Piotr Nowak",
+        "email": "piotr@example.com",
+        "addresses": [
+            {"city": "Warszawa", "street": "Długa 12"}
+        ],
+        "interests": ["music", "python"]
+    }
+]
