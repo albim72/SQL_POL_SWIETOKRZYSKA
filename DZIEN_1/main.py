23d869256a11bd89d69d5d821ac1deeaf2daa063
albim72 <marcin.albiniak72@gmail.com>
2026-03-26 12:16:06 +0100

    Update main.py

--- DZIEN_1/main.py
+++ DZIEN_1/main.py
@@ -20,6 +20,28 @@ def main():
     insert(collection, new_user)
     print_collection("po dodaniu użykownika", collection)
 
+    print("\nFIND ONE")
+    user = find_one(collection,"user_id",2)
+    print(user)
+
+    print("\nUPDATE")
+    upadated = update_one(collection,"user_id",1,{"email":"anna.kowalska@exaple.com"})
+    print("Zaktualizowano",upadated)
+    print_collection("po aktualizacji", collection)
+
+    print("\nDELETE")
+    deleted = delete_one(collection,"user_id",2)
+    print("usunięto",deleted)
+    print_collection("po usunięciu", collection)
+
+    print("\nSAVE TO JSON")
+    save_to_json("data/users.json",collection)
+    print("plik zapisany")
+
+    print("\nLOAD FROM JSON")
+    loaded = load_from_json("data/users.json")
+    print_collection("wczytane dane",loaded)
+
 # if __name__ == "__main__":
 if  "__main__"==__name__:
     main()
