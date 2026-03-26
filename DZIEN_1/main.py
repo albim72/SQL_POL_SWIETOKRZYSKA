11c204ed7b1d2f3426f66c813fe19219efefc0a4
albim72 <marcin.albiniak72@gmail.com>
2026-03-26 11:48:49 +0100

    Update main.py

--- DZIEN_1/main.py
+++ DZIEN_1/main.py
@@ -1,8 +1,25 @@
 from sample_data import users
+from crud import insert, find_all, find_one, update_one, delete_one
+from storage import save_to_json, load_from_json
+from utils import print_collection, build_index
 
 def main():
     collection = users.copy()
-    print(collection)
+    print_collection("users", collection)
 
-if __name__ == "__main__":
+    new_user  = {
+        "user_id": 3,
+        "name": "Ewa Zielińska",
+        "email": "ewa@example.com",
+        "addresses": [
+            {"city": "Lublin", "street": "Polna 7"}
+        ],
+        "interests": ["ethics", "databases", "books"]
+    }
+    print("\nINSERT")
+    insert(collection, new_user)
+    print_collection("po dodaniu użykownika", collection)
+
+# if __name__ == "__main__":
+if  "__main__"==__name__:
     main()
